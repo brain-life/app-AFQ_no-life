b02618527c6b17b4d18b26d76c9af91c21fd205d
Soichi Hayashi <hayashis@iu.edu>
2018-02-14 11:11:03 -0500

    added spm8 to the path

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/afq'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/encode'))
+addpath(genpath('/N/soft/mason/SPM/spm8'))
+
 mcc -m -R -nodisplay -d compiled main
 exit
 END
