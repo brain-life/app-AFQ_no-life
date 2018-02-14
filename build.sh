5d38a8418969cf55faa5403fc133276dda8c896b
Soichi Hayashi <hayashis@iu.edu>
2018-02-14 17:05:00 -0500

    loading spm via module

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 module load matlab/2017a
+module load spm
 
 cat > build.m <<END
+addpath(genpath('/N/u/brlife/git/encode'))
+addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/afq'))
-addpath(genpath('/N/u/brlife/git/vistasoft'))
-addpath(genpath('/N/u/brlife/git/encode'))
-addpath(genpath('/N/soft/mason/SPM/spm8'))
 
 mcc -m -R -nodisplay -d compiled main
 exit
