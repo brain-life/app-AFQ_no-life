039d395f8852d4b756f045acedadd9ba01d57290
Soichi Hayashi <hayashis@iu.edu>
2018-02-14 18:00:04 -0500

    added addpath for spm8 as well as module load

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/afq'))
+addpath(genpath('/N/soft/mason/SPM/spm8'))
 
 mcc -m -R -nodisplay -d compiled main
 exit
