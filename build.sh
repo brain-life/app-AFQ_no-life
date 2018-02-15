fe9b50500a2416667b99e5837943a3e03efe5850
Soichi Hayashi <hayashis@iu.edu>
2018-02-14 21:42:55 -0500

    updated to use lindsey's patched vistasoft/afq

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 module load matlab/2017a
-module load spm
+#module load spm
 
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/encode'))
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+%addpath(genpath('/N/u/brlife/git/vistasoft'))
+%addpath(genpath('/N/u/brlife/git/afq'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/u/brlife/git/afq'))
-addpath(genpath('/N/soft/mason/SPM/spm8'))
+addpath(genpath('/N/soft/rhel7/spm/8'))
+
+%need to use lindsey's patched version to work around the spm issue
+addpath(genpath('/N/u/kitchell/Karst/Applications/vistasoft'))
+addpath(genpath('/N/u/kitchell/Karst/Applications/AFQ'))
 
 mcc -m -R -nodisplay -d compiled main
 exit
