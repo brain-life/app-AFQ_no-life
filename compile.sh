a7bba4d0a80a1a32083efd9f8f737adf4c0d78ee
Soichi Hayashi <hayashis@iu.edu>
2018-05-22 18:19:27 -0400

    renamed build.sh to compile.sh

--- compile.sh
+++ compile.sh
@@ -3,8 +3,6 @@ module load matlab/2017a
 
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/encode'))
-%addpath(genpath('/N/u/brlife/git/vistasoft'))
-%addpath(genpath('/N/u/brlife/git/afq'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/rhel7/spm/8'))
 
